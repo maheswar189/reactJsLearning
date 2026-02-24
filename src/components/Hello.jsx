d4e512a539f2198057687a4e7768bad60d77298c
maheswar189 <maheswarreddy189@gmai.com>
2026-02-24 08:17:29 +0530

    Destructring pros

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -5,9 +5,10 @@ function displayName() {
 }
 function Hello(props) {
   console.log(props);
+  const { name, message } = props;
   return (
     <h1>
-      Hello {props.message} {props.name}! from Hello.jsx
+      Hello {name} {message} ! from Hello.jsx
     </h1>
   );
 }
