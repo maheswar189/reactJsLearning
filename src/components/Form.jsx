e3148a7f02588da608a37e309253c166673f6bbf
maheswar189 <maheswarreddy189@gmai.com>
2026-06-04 07:33:53 +0530

    Updating to lambda expression and setName function is activated

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 
 export default function Form() {
+  const [name, setName] = useState("");
   function handleChange(e) {
-    //const [name, setName] = useState("");
-    console.log(e.target.value);
+    // console.log(e.target.value);
+    setName(e.target.value);
   }
 
   return (
     <div>
       <form>
+        <h1>FORM:::</h1>
         <input
-          onChange={function demo(e) {
-            return handleChange(e);
+          //  onChange={function demo(e) {
+          //   return handleChange(e);
+          // }}
+          onChange={(e) => {
+            handleChange(e);
           }}
           type="text"
           value={name}
